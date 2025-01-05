1cd6eb915472ef46c62a4fb7902bc9d556f0645f
dimdung <dimdung@live.com>
2025-01-05 01:42:43 -0500

    Create awsou.tf

---
+++ awsou.tf
@@ -0,0 +1,16 @@
+# OUs are manually created (not created by Terraform)  
+# This can be used to re-create OU structure from Terraform
+resource "aws_organizations_organizational_unit" "HELIX" {
+   name      = "HELIX"
+   parent_id = aws_organizations_organization.example.roots[0].id
+ }
+
+ resource "aws_organizations_organizational_unit" "workloads" {
+   name      = "Workloads"
+   parent_id = aws_organizations_organizational_unit.helix.id
+ }
+
+ resource "aws_organizations_organizational_unit" "platform" {
+   name      = "Platform"
+   parent_id = aws_organizations_organizational_unit.helix.id
+ }
