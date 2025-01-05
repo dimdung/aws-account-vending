d7dad7b2156af83c70794a095dd2e9d4f8e4fad7
dimdung <dimdung@live.com>
2025-01-05 01:46:09 -0500

    Update awsou.tf

--- awsou.tf
+++ awsou.tf
@@ -1,5 +1,10 @@
-i# OUs are manually created (not created by Terraform)  
+# OUs are manually created (not created by Terraform)  
 # This can be used to re-create OU structure from Terraform
+
+## AWS Organization 
+data "aws_orginizations_organization" "cqpocs"{}
+
+## Below are the OU
 resource "aws_organizations_organizational_unit" "helix" {
    name      = "HELIX"
    parent_id = aws_organizations_organization.example.roots[0].id
